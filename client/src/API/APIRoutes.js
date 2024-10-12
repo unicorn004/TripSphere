cd916db92345e409d2de6a51a6426c5e4a1b7d3f
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-10-12 15:17:07 +0530

    integrated chatbot

--- client/src/API/APIRoutes.js
+++ client/src/API/APIRoutes.js
@@ -1,3 +1,7 @@
 const host = import.meta.env.VITE_BACKEND_URL
 
 export const setCityEndpoint = `${host}/user/city`
+export const getMessagesEndpoint = `${host}/message/get`
+export const addMessageEndpoint = `${host}/message/add`
+export const deleteChatsEndpoint = `${host}/message/delete`
+export const geminiApi = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key"
