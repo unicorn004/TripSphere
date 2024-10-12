911ad8f002b498b1ed3f8fedfe8d0944fa03c76a
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-10-12 15:17:07 +0530

    integrated chatbot

---
+++ client/src/components/NoChat.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../public/logo.png'
+
+const NoChat = () => {
+    return (
+        <div className='flex flex-col justify-center items-center h-full'>
+            <img src={logo} alt="" />
+            <p className='font-bold text-4xl text-yellow-500'>Talk with Elisa,</p>
+            <br />
+            <p className='font-bold text-2xl text-yellow-500'>Your personal travel guide.</p>
+        </div>
+    )
+}
+
+export default NoChat
